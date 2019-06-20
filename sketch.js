25bb753fbcabb9c85d2cdb0cae36ea8c536d200b
RossHoyt <rosshoytmusic@gmail.com>
2019-06-19 21:51:21 -0700

    simple bw color scheme

--- sketch.js
+++ sketch.js
@@ -3,6 +3,10 @@ const NOTENAMES_SHARP =
 				[ "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", ];
 const NOTENAMES_FLAT =
 				[ "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B", ];
+const TEXT_XOFFSETS =
+				[ ];
+const TEXT_YOFFSETS =
+				[ ];
 
 // View Vars
 let viewSharps = true; // Default to show Sharps
@@ -16,37 +20,43 @@ function setup() {
 
 
 function draw() {
-  background(0, 0, 200);
-	textSize(15);
+  background(0, 0, 0);
+
 
 	// Set circle dimensions
 	let circleX = windowWidth / 2;
 	let circleY = windowHeight / 2;
-	let circleDiameter = (circleY > circleX) ? circleX : circleY;
+	let circleDiameter = ((circleY > circleX) ? circleX : circleY) * 2 / 3;
 	let circleRadius = circleDiameter / 2;
 	// Set circle color
-	let c = color(255, 204, 0);
+	let c = color(190, 190, 190);
 	fill(c);
 	noStroke();
 	// Paint background circle
 	circle(circleX, circleY, circleDiameter);
 
+	// TEXT PROPS
+	let textColor = color(255, 255, 255);
+	fill(textColor);
+	noStroke();
+	textSize(40);
 
 
-	let startingLocation = 0;
+	// Positional Variables
+	let textOffset = 10;
+	let startingLocation = 9;
 	let counter = 0;
 	for(let i = 0; i <= 12; i++) {
 			let angle = slice * startingLocation;
 			let newX = circleX + circleDiameter * Math.cos(angle);
       let newY = circleY + circleDiameter * Math.sin(angle);
-			// Draw Line
-			line(circleX, circleY, newX, newY);
+			// Draw Line if present in chord (development state = true)
+			if(true)line(circleX, circleY, newX, newY);
 			stroke(0);
-			// Draw noteName
-			//var noteNames = viewSharps ? NOTENAMES_SHARP : NOTENAMES_FLAT;
-			//text();
-
-
+			// Print noteName
+			var noteNames = viewSharps ? NOTENAMES_SHARP : NOTENAMES_FLAT;
+			//text(noteNames[i], newX - TEXT_XOFFSETS[i], newY + TEXT_YOFFSETS[i]);
+			text(noteNames[i], newX - textOffset, newY + textOffset);
 			startingLocation = (startingLocation + 1) % 12;
 	}
 }
