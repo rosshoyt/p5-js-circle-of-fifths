809aaee92e5a339c93698b8257ab4bf433a8de00
RossHoyt <rosshoytmusic@gmail.com>
2019-06-19 22:44:32 -0700

    responsive keys paint co5 - problem painting [0] (C)

--- sketch.js
+++ sketch.js
@@ -8,11 +8,17 @@ const TEXT_XOFFSETS =
 const TEXT_YOFFSETS =
 				[ ];
 
+// Sound params
+//let notesPressed = new Array(12).fill(false);
+let notesPressed = new Set();
+
+let KEY_PRESSED_VALS = [ 65, 87, 83, 69, 68, 70, 84, 71, 89, 72, 85, 74, 75 ];
+
 // View Vars
 let viewSharps = true; // Default to show Sharps
 let buttonFlat, buttonSharp;
 // Circleoffifths dimension Vars
-slice = 2 * Math.PI / 12;
+let slice = 2 * Math.PI / 12;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
@@ -51,7 +57,7 @@ function draw() {
 			let newX = circleX + circleDiameter * Math.cos(angle);
       let newY = circleY + circleDiameter * Math.sin(angle);
 			// Draw Line if present in chord (development state = true)
-			if(true)line(circleX, circleY, newX, newY);
+			if(notesPressed.has(i))line(circleX, circleY, newX, newY);
 			stroke(0);
 			// Print noteName
 			var noteNames = viewSharps ? NOTENAMES_SHARP : NOTENAMES_FLAT;
@@ -59,4 +65,22 @@ function draw() {
 			text(noteNames[i], newX - textOffset, newY + textOffset);
 			startingLocation = (startingLocation + 1) % 12;
 	}
+}
+
+function keyPressed() {
+	if(KEY_PRESSED_VALS.includes(keyCode)){
+		//notesPressed[KEY_PRESSED_VALS.indexOf(keyCode)] == true;
+		notesPressed.add(KEY_PRESSED_VALS.indexOf(keyCode));
+		console.log("Scale key pressed #" + KEY_PRESSED_VALS.indexOf(keyCode));
+		console.log(notesPressed);
+	} else return false;
+}
+function keyReleased() {
+	if(KEY_PRESSED_VALS.includes(keyCode)){
+		//notesPressed[KEY_PRESSED_VALS.indexOf(keyCode)] == false;
+		notesPressed.delete(KEY_PRESSED_VALS.indexOf(keyCode));
+		console.log("Scale key released");
+		console.log(notesPressed);
+	} else return false;
+	return false;
 }
